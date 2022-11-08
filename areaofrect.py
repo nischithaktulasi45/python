cb9b9c0322b5ab6abcfeb07d8ee0133dedc79a7d
nischithaktulasi45 <nischithaktulasi45@gmail.com>
2022-11-08 13:04:26 -0800

    first change

---
+++ areaofrect.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 08:47:34 2022
+
+@author: SPTINT-13
+"""
+
+width = float(input('Please enter the width: '))
+height = float(input('Please enter the height: '))
+area = width  * height
+perimeter = 2 * (width + height)
+print("rectangles area is: ", area )
+print("rectangles perimeter is: ", perimeter )
